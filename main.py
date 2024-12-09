9ff1272a32ee5754f750802cfb08da4889e2a723
Anu Philip <79272418+anuphilip472@users.noreply.github.com>
2024-12-09 23:43:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 
+import numpy as np 
+import pandas as pd 
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+      
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import re
+import nltk
+import string
+from nltk.corpus import stopwords
+from wordcloud import WordCloud, STOPWORDS
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from nltk.tokenize import RegexpTokenizer
+
+from imblearn import under_sampling, over_sampling
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.naive_bayes import MultinomialNB
+
+df = pd.read_csv("/kaggle/input/GenshinImpact_30Maret2024_MostRelevant_non995.csv")
+df = df[[
+    'content',
+    'sentiment'
+]]
+df['sentiment'].value_counts()
